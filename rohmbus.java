15ef051073ed79c47a6930b37c16603ab9464433
asmitajadhav2006 <jadhavasmita548@gmail.com>
2026-09-02 21:13:32 +0530

    JAVA

---
+++ rohmbus.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class rohmbus {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars and spaces
+            for (int j = 1; j <= 2 * i - 1; j++) {
+
+                if (j == 1 || j == 2 * i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+
+        // Lower half
+        for (int i = n - 1; i >= 1; i--) {
+
+            // Print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars and spaces
+            for (int j = 1; j <= 2 * i - 1; j++) {
+
+                if (j == 1 || j == 2 * i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
