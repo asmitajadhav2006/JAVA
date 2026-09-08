dcfd2f90b9769f6d45ce9743efda7c308606f778
asmitajadhav2006 <jadhavasmita548@gmail.com>
2026-09-08 22:47:36 +0530

    JAVA

---
+++ Stringbasis.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+public class Stringbasis{
+    public static void main(String[]args){
+
+   /* String FirstName="ASHU";
+    String LastName=new String("jadhav");
+    System.out.println(FirstName.length());
+    System.out.println(LastName.charAt(
+        0));
+}
+//String can be mutable using string builder and stringpool
+
+String name="Rana";
+name="Bana";
+System.out.println(" "+name);
+}
+} 
+//comparision of string by three metho 1.== 2..equals() 3.equalignore case()
+
+        String name1 = "Love";
+        String name2 = "Love";
+
+        if (name1 == name2) {
+            System.out.println("Both the strings are same");
+        } else {
+            System.out.println("Both the strings are not same");
+        }
+    }
+
+        String name1 = "Love";
+        String name2 = "LOVE";
+
+        if (name1.equals(name2)) {
+            System.out.println("Both the strings are same");
+        } else {
+            System.out.println("Both the strings are not same");
+        }
+    }
+}}
+String name1 = "Love";
+        String name2 = "LOVE";
+
+        if (name1.equalsIgnoreCase(name2)) {
+            System.out.println("Both the strings are same");
+        } else {
+            System.out.println("Both the strings are not same");
+        }
+    }}
+*/
+//string input 
+Scanner sc = new Scanner(System.in);
+System.out.println("PROVIDE THE STRING CONTENT");
+String str = sc.nextLine();
+System.out.println("VALUE OF nextline "+str);
+System.out.println("PROVIDE THE STRING CONTENT");
+String str2 = sc.next();    
+System.out.println("VALUE OF next "+str2);
+}
+}
