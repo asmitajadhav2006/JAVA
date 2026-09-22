b3b995259f9ebff14b764bbaaef161604fa81e35
asmitajadhav2006 <jadhavasmita548@gmail.com>
2026-09-22 23:27:05 +0530

    JAVA

--- largest.java
+++ largest.java
@@ -26,6 +26,35 @@ public class largest {
         System.out.println("Largest = " + largest);*/
 
     
+    /*    Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = arr[0];
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = 1; i < n; i++) {
+
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            }
+            else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        System.out.println("Second Largest = " + secondLargest);*/
+        
+
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter array size: ");
@@ -52,6 +81,7 @@ public class largest {
             }
         }
 
+        System.out.println("Largest = " + largest);
         System.out.println("Second Largest = " + secondLargest);
     }
 }
