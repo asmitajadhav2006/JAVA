b6b45854555c7a16a8187027744b8ec8ab85c482
asmitajadhav2006 <jadhavasmita548@gmail.com>
2026-09-24 18:47:25 +0530

    JAVA

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) {// min max  in  A SINGLE TRAVERSAL
      /*    Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter size: ");
@@ -29,6 +29,7 @@ public class Main {
 
         System.out.println("Maximum = " + max);
         System.out.println("Minimum = " + min);*/
+        // COPY ARRAY INTO ANOTHER ANOTHER
         /*Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter size: ");
@@ -53,9 +54,9 @@ public class Main {
             System.out.print(arr2[i] + " ");
         }
     }
-}*/
+}*///ALTERNATE SWAP OF ELEMENTS
 
-        Scanner sc = new Scanner(System.in);
+      /* Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter size: ");
         int n = sc.nextInt();
@@ -79,5 +80,62 @@ public class Main {
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
+    }
+}*//*5. Find Unique Element Approach 1 — Replace Duplicate With 0*/
+
+       /*  Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            if (arr[i] == 0) {
+                continue;
+            }
+
+            for (int j = i + 1; j < n; j++) {
+
+                if (arr[i] == arr[j]) {
+                    arr[i] = 0;
+                    arr[j] = 0;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                System.out.println("Unique element = " + arr[i]);
+            }
+        }
+    }
+}*/
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int unique = 0;
+
+        for (int i = 0; i < n; i++) {
+            unique = unique ^ arr[i];
+        }
+
+        System.out.println("Unique element = " + unique);
     }
 }
