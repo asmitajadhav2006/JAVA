9006d95bfc80c47bf09905db30410f297dc24572
asmitajadhav2006 <jadhavasmita548@gmail.com>
2026-09-24 18:25:33 +0530

    JAVA

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+     /*    Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter size: ");
         int n = sc.nextInt();
@@ -28,6 +28,29 @@ public class Main {
         }
 
         System.out.println("Maximum = " + max);
-        System.out.println("Minimum = " + min);
+        System.out.println("Minimum = " + min);*/
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n];
+
+        System.out.println("Enter elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr1[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr2[i] = arr1[i];
+        }
+
+        System.out.println("Copied array:");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr2[i] + " ");
+        }
     }
 }
